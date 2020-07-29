09cf8b238c9423a364539329bb66ac7eb70e2340
DariuszAftanski <dariusz.aftanski@outlook.com>
2020-07-29 20:26:47 +0100

    Third commit to shell sort

--- Shell_sort/code.c
+++ Shell_sort/code.c
@@ -5,10 +5,15 @@
 #define LENGTH 6 // Length of the tested array:
 
 void shell_sort(int * array, int length) {
+    // There are three nested loops in this algorithm.
     int temp_var;
 
+    // First for loop controls the gap between the compared elements:
     for (int gap = length/2; gap > 0; gap /= 2) {
+        // Second for loop steps along the elements:
         for (int i = gap; i < length; i++) {
+            // Third for loop compares each pair of elements that is seperated...
+            // ... and reverse any out of order.
             for (int j = i - gap; j >= 0 && array[j] > array[j + gap]; j -= gap) {
                 temp_var = array[j];
                 array[j] = array[j + gap];
@@ -19,16 +24,24 @@ void shell_sort(int * array, int length) {
 }
 
 int main(void) {
+    // Declare an array:
     int array_integers[LENGTH];
+
+    // Populate the array:
     for (int i = 0; i < LENGTH; i++) {
         array_integers[i] = rand() % 100;
         printf("%d \t", array_integers[i]);
     }
     printf("\n");
+
+    // Apply the sorting algorithm:
     shell_sort(array_integers, LENGTH);
+
+    // Display the sorted array:
     for (int i = 0; i < LENGTH; i++) {
         printf("%d \t", array_integers[i]);
     }  
     printf("\n");
+
     return 0;
 }
